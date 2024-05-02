64894d2abba72645a961de2449691c98797551e2
khalil <khalilibrahim.haouari@esprit.tn>
2024-05-02 04:25:42 +0100

    made changes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,19 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use  Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
+use App\Controller\DefaultController;
+use Doctrine\ORM\EntityManagerInterface;
+
 class UserController extends AbstractController
 {
+    private $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
+        return $this->render('base2.html.twig', [
             'controller_name' => 'UserController',
         ]);
     }
+    #[Route('/read', name: 'user_read')]
+    public function read(ManagerRegistry $doctrine): Response
+    {
+        $user = $doctrine->getRepository(User::class)->findAll();
+        return $this->render('user/read.html.twig',
+            ["user" => $user]);
+    }
+    #[Route('/allusers', name: 'admin_read')]
+    public function readAdmin(ManagerRegistry $doctrine): Response
+    {
+       
+        $user = $this->userRepository->findAll();
+        return $this->render('user/readAdmin.html.twig',
+            ["user" => $user]);
+    }
+
+    #[Route('/edit', name: 'user_edit')]
+    public function edit(Request $request)
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Save the updated user details to the database
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('user_profile');
+        }
+
+        return $this->render('user/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route("/deleteUser/{id}", name:'user_delete')]
+    public function delete(User $user,EntityManagerInterface $em)
+    {
+        $em->remove($user);
+        $em->flush();
+    }
+
+
 }
-
-
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,72 +4,57 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $username = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $lastname = null;
-
-    #[ORM\Column(length: 255)]
-    private ?string $password = null;
-
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message:"Veuillez remplir ce champ")]
+    #[Assert\Email(message:"Mail invalide")]
     private ?string $email = null;
 
     #[ORM\Column]
-    private ?int $type = null;
+    private array $roles = [];
+
+    /**
+     * @var string The hashed password
+     */
+    #[ORM\Column]
+    private ?string $password = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Veuillez remplir ce champ")]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Veuillez remplir ce champ")]
+    private ?string $nom = null;
 
     #[ORM\Column]
-    private ?int $connected = null;
+    #[Assert\Length(min:8,max:8)]
+    #[Assert\NotBlank(message:"Veuillez remplir ce champ")]
+    private ?int $tel = null;
+
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomSup = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresseSup = null;
 
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getUsername(): ?string
-    {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): static
-    {
-        $this->username = $username;
-
-        return $this;
-    }
-
-    public function getLastname(): ?string
-    {
-        return $this->lastname;
-    }
-
-    public function setLastname(string $lastname): static
-    {
-        $this->lastname = $lastname;
-
-        return $this;
-    }
-
-    public function getPassword(): ?string
-    {
-        return $this->password;
-    }
-
-    public function setPassword(string $password): static
-    {
-        $this->password = $password;
-
-        return $this;
     }
 
     public function getEmail(): ?string
@@ -77,33 +62,141 @@ class User
         return $this->email;
     }
 
-    public function setEmail(string $email): static
+    public function setEmail(string $email): self
     {
         $this->email = $email;
 
         return $this;
     }
 
-    public function getType(): ?int
+    /**
+     * A visual identifier that represents this user.
+     *
+     * @see UserInterface
+     */
+    public function getUserIdentifier(): string
     {
-        return $this->type;
+        return (string) $this->nomSup;
     }
 
-    public function setType(int $type): static
+    /**
+     * @deprecated since Symfony 5.3, use getUserIdentifier instead
+     */
+    public function getUsername(): string
     {
-        $this->type = $type;
+        return (string) $this->email;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        // guarantee every user at least has ROLE_USER
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
 
         return $this;
     }
 
-    public function getConnected(): ?int
+    /**
+     * @see PasswordAuthenticatedUserInterface
+     */
+    public function getPassword(): string
     {
-        return $this->connected;
+        return $this->password;
     }
 
-    public function setConnected(int $connected): static
+    public function setPassword(string $password): self
     {
-        $this->connected = $connected;
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Returning a salt is only needed, if you are not using a modern
+     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
+     *
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials()
+    {
+        // If you store any temporary, sensitive data on the user, clear it here
+        // $this->plainPassword = null;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getTel(): ?int
+    {
+        return $this->tel;
+    }
+
+    public function setTel(int $tel): self
+    {
+        $this->tel = $tel;
+
+        return $this;
+    }
+
+    public function getNomSup(): ?string
+    {
+        return $this->nomSup;
+    }
+
+    public function setNomSup(string $nomSup): self
+    {
+        $this->nomSup = $nomSup;
+
+        return $this;
+    }
+
+    public function getAdresseSup(): ?string
+    {
+        return $this->adresseSup;
+    }
+
+    public function setAdresseSup(string $adresseSup): self
+    {
+        $this->adresseSup = $adresseSup;
 
         return $this;
     }
